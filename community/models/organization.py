935078e3ac78249ea7c3ba157a6c40bef732aa4a
Juho Kim <juho-kim@outlook.com>
2020-10-09 02:05:52 -0400

    Cleaned Up Community

--- community/models/organization.py
+++ community/models/organization.py
@@ -5,6 +5,7 @@ from django.db.models import Model, CharField, ManyToManyField
 class Organization(Model):
     name = CharField(max_length=255, unique=True)
     description = CharField(max_length=255, blank=True)
+
     members = ManyToManyField(get_user_model(), related_name="organizations", blank=True)
 
     def __str__(self):
